08efd34c68cd89a88bb2b1f1bb13dfec3548fb4c
nieminglan <704640380@qq.com>
2024-08-19 18:21:21 +0800

    TestA

---
+++ src/main/java/com/cgtn/demo/common/TestA.java
@@ -0,0 +1,18 @@
+package com.cgtn.demo.common;
+
+/**
+ * @BelongsProject: demo-java-project
+ * @BelongsPackage: com.cgtn.demo.common
+ * @ClassName TestA
+ * @Author: nieminglan
+ * @CreateTime: 2024-08-19  18:20
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class TestA {
+
+    public static void main(String[] args) {
+        System.out.println("TestA");
+    }
+
+}
